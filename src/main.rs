9964ea3f13dd37dfa427becb2c10fada6863c24a
Matt Muldowney <matt.muldowney@gmail.com>
2023-09-24 00:03:42 -0500

    working librespot stuff

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,64 @@
-use rspotify::{
-    prelude::*,
-    scopes,
-    model::{PlayableItem, AdditionalType, Country, Market, CurrentlyPlayingContext},
-    AuthCodeSpotify,
-    Credentials,
-    OAuth,
+use std::{env, process::exit};
+
+use librespot_core::{
+    authentication::Credentials,
+    config::SessionConfig,
+    session::Session,
+    spotify_id::{SpotifyId, SpotifyAudioType},
+};
+use librespot_playback::{
+    audio_backend,
+    config::{AudioFormat, PlayerConfig},
+    mixer::NoOpVolume,
+    player::Player,
 };
 
 #[tokio::main]
 async fn main() {
-    let creds = Credentials::from_env().unwrap();
-    let oauth = OAuth::from_env(scopes!("user-read-currently-playing")).unwrap();
-    let spotify = AuthCodeSpotify::new(creds, oauth);
-    let url = spotify.get_authorize_url(false).unwrap();
+    let args: Vec<String> = env::args().collect();
+    let username = args[1].clone();
+    let password = args[2].clone();
 
-    spotify.prompt_for_token(&url).await.unwrap();
+    let mut track = SpotifyId::from_base62(&args[3]).unwrap();
+    track.audio_type = SpotifyAudioType::Track;
 
-    let market = Market::Country(Country::UnitedStates);
-    let additional_types = [AdditionalType::Track];
-    let current_playing = spotify
-        .current_playing(Some(market), Some(&additional_types))
-        .await
-        .unwrap();
+    //for (backend_name, _) in audio_backend::BACKENDS {
+    //    println!("{backend_name}");
+    //}
+    //exit(1);
+    let (backend_name, backend) = &audio_backend::BACKENDS[0];
 
-    if let Some(context) = current_playing {
-        if let Some(PlayableItem::Track(full_track)) = context.item {
-            let artist = &full_track.artists[0].name;
-            let track = &full_track.name;
+    let session_config = SessionConfig::default();
+    let player_config = PlayerConfig::default();
+    let audio_format = AudioFormat::default();
 
-            println!("{artist}: {track}");
-        }
+    println!("Connecting....");
+    if let Ok((session, _)) = Session::connect(
+        session_config,
+        Credentials::with_password(username, password),
+        None,
+        false
+    ).await {
+        let (mut player, _) = Player::new(
+            player_config,
+            session,
+            Box::new(NoOpVolume),
+            move || {
+                println!("{backend_name}");
+                backend(None, audio_format)
+            }
+        );
+
+        player.load(track, true, 0);
+        //player.play();
+
+        println!("Playing...");
+
+        player.await_end_of_track().await;
+
+        println!("Done");
 
     }
-
 }
+
+
